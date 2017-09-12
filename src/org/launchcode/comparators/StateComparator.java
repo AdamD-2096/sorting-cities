a7e93f12debebe5668bc2a65ec2db94550fcebb8
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-09-11 20:45:05 -0700

    jumped the gun. fixed

--- src/org/launchcode/comparators/StateComparator.java
+++ src/org/launchcode/comparators/StateComparator.java
@@ -8,6 +8,6 @@ public class StateComparator implements Comparator<City> {
 
     @Override
     public int compare(City o1, City o2) {
-        return o1.getName().compareTo(o2.getName());
+        return o1.getState().compareTo(o2.getState());
     }
 }
