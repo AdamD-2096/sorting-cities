2c20ed21571741b0df435b687035184849405cbd
Adam Duhamel <Adam.Duhamel1996@gmail.com>
2017-09-11 20:43:44 -0700

    Implemented State comparator

--- src/org/launchcode/Main.java
+++ src/org/launchcode/Main.java
@@ -1,17 +1,35 @@
 package org.launchcode;
 
 import org.launchcode.comparators.NameComparator;
+import org.launchcode.comparators.StateComparator;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ArrayList<City> cities = CityData.loadData();
+        Comparator<? super City> comparator;
 
         // TODO - Use different comparators here
-        NameComparator comparator = new NameComparator();
+        Scanner in = new Scanner(System.in);
+        System.out.println("would you like to organize by name(1), state(2), population(3), or square miles(4)?");
+        int choice = in.nextInt();
+
+        if (choice == 1) {
+            comparator = new NameComparator();
+        }else if (choice == 2) {
+            comparator = new StateComparator();
+        }else if (choice == 3){
+            System.out.println("Not implemented yet");
+            comparator = new NameComparator();
+        }else{
+            System.out.println("Not implemented yet");
+            comparator = new NameComparator();
+        }
         cities.sort(comparator);
 
         printCities(cities);
